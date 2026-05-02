14fac2515f97fa320d1db4e16fa53b5cf79047b1
PBalanjineyulu <balanjineyulu03@gmail.com>
2026-05-02 13:59:15 +0530

    clean repo - ignore config, uploads, venv

--- config.py
+++
@@ -1,9 +0,0 @@
-SECRET_KEY = "secret123"
-
-MAIL_USERNAME = "balanjineyulu03@gmail.com"
-MAIL_PASSWORD = "your_new_gmail_app_password"
-
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = "Gopi@4106"
-DB_NAME = "notesapp"
